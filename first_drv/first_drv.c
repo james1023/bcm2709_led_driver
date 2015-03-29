b9020c371d35d20798ff41884cf8b3883e48fa17
James Huang <james.huang1023@gmail.com>
2015-03-29 17:59:02 +0800

    add linux/kernel.h.

--- first_drv/first_drv.c
+++ first_drv/first_drv.c
@@ -1,7 +1,8 @@
+
+#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
-#include <linux/init.h>
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <asm/uaccess.h>
--- hello/hello/hello.c
+++ hello/hello/hello.c
@@ -1,6 +1,16 @@
+
+
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/delay.h>
+#include <linux/device.h>
 #include <linux/timer.h>
+#include <asm/uaccess.h>
+#include <asm/irq.h>
+#include <asm/io.h>
+
 
 volatile unsigned long *gpio = NULL;
 
